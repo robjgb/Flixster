713904da464534754fb3a0b6fc8d7d3a4dd9fc91
Robert Balatbat <robjb822@gmail.com>
2021-09-17 08:21:42 -0700

    commit

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -24,7 +24,6 @@ import okhttp3.Headers;
 public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
-    public static final String GENRES_URL = "https://api.themoviedb.org/3/genre/movie/list?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
 
     List<Movie> movies;
